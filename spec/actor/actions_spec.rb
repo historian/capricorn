79a5807c92bee695cdea502fee19419bb6831a38
Simon Menke <simon@mrhenry.be>
2009-06-02 17:45:26 +0200

    added spec dir

---
+++ spec/actor/actions_spec.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe 'Actor::Actions' do
+  
+  before do
+    @actor = Class.new('Actor')
+  end
+  
+  it "should description" do
+    
+  end
+  
+end
